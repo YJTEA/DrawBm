74e1cb9cab1dac9cf8643ccdbc58453afab44acc
Sasaki Moe <sasakimoe@sasaki-no-MacBook-Air.local>
2017-10-26 23:25:26 +0900

    Data Insert Part

---
+++ Pos.java
@@ -0,0 +1,16 @@
+package in.andante.drawbm;
+	
+//PosListへ格納//
+public class Pos {
+
+	public float X;
+	public float Y;
+	public float pressure;
+
+public Pos(float X, float Y, float pressure){//引数の座標および筆圧を代入
+	this.X = X;
+	this.Y = Y;
+	this.pressure = pressure;
+}
+
+}
