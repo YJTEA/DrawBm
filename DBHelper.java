e32f8bfc120344cb7c9bae257ed862644c19ac7a
Sasaki Moe <sasakimoe@sasaki-no-MacBook-Air.local>
2017-11-23 14:33:53 +0900

    11_23

--- DBHelper.java
+++ DBHelper.java
@@ -5,11 +5,16 @@ package in.andante.drawbm;
 import java.util.ArrayList;
 import java.util.List;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.Toast;
 import in.andante.drawbm.PenView;
+import android.app.Activity;
+import in.andante.drawbm.Exchange;
+import android.app.Application;
 
 public class DBHelper extends SQLiteOpenHelper {
 	
@@ -17,15 +22,18 @@ public class DBHelper extends SQLiteOpenHelper {
 	private static final int DB_VERSION = 1;
 	private static final String DB_NAME ="DrawBmDBv.db";
 	public Activity _context;
+	public Activity __context;
 	public List<Pos> posList;//Posクラスの座標データを扱うリスト
 	PenView penview;
+	//String str = "sbr";
 
 public DBHelper( Context context ){//コンストラクタ
 		
 	super(context, DB_NAME, null, DB_VERSION);
 	_context = (Activity)context;
-	penview = new PenView(_context);
-	penview.posList = new ArrayList<Pos>();
+	//penview = new PenView(_context);
+	//penview.posList = new ArrayList<Pos>();
+	__context = (Activity)context;
 		
 }
 	
@@ -34,17 +42,15 @@ public void onCreate(SQLiteDatabase db) {
 	
 		db.beginTransaction();//トランザクション処理開始
 		
-		/*テーブル作成?*/
+		/*テーブル作成*/
 		try{
-			db.execSQL(
-					"CREATE TABLE PersonalData(" +
-					"   xpoint INTEGER," +
-					"   ypoint INTEGER," +
-					"   zpressure INTEGER" +
-					")"
-			);
+			String sql = "create table PersonalData "+ " ("
+		            + " X INTEGER, "
+		            + " Y INTEGER, "
+		            +");";
+			db.execSQL(sql);
 	        db.setTransactionSuccessful();//成功
-	        Log.i(TAG,"テーブルが作成されました");
+	        Toast.makeText(_context, "Table Create!", Toast.LENGTH_SHORT).show();  
 		} 
 		catch(Exception e){//例外発生
 			e.printStackTrace();
@@ -53,6 +59,33 @@ public void onCreate(SQLiteDatabase db) {
 	        db.endTransaction();//トランザクション処理終了
 		}
 		
+}
+
+/*SQLiteへデータ追加*/
+public void insert(){
+	String x = "X";
+	String y = "Y";
+	
+	//Exchange exchange = (Exchange) __context.getApplication();
+    //String str = exchange.getTestString();
+    
+	ContentValues values = new ContentValues();//テーブルに含まれるカラムをキーとし、カラムに対して設定したい値をペアとして保存する
+	
+	//データ追加部分
+	values.put(x, 1);
+	values.put(y, 2);
+
+	DBHelper db = new DBHelper(_context);  
+	SQLiteDatabase db1 = db.getWritableDatabase();//読み書き用
+	
+    try{
+    	db1.insert("PersonalData", null, values);
+    }
+    finally{
+        db1.close();
+        Toast.makeText(_context, "Insert!", Toast.LENGTH_SHORT).show();  
+    }
+    
 }
 	
 /*SQL取り出し部分
